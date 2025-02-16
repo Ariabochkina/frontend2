c7c05407bb6aa2953ea118e3d68c6fa1e7cdc376
Amriabochkina <Anna.rm07@mail.ru>
2025-02-16 19:00:06 +0300

    addition doesn't work

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,43 +3,69 @@ import Recipe from '../Components/Recipe'
 export class Home extends Component {
     constructor(props){
         super(props)
-        this.state = {json: null, recipes: null}
+        this.state = {json: null, recipes: null, recipeAccess: []}
         this.state.json = [{
             name: "espresso", 
             tastes: ["bitter", "sour", "strong"],
-            default_measures: {ground_coffee: 18, water: 36},
-            change_coeficients: {ground_coffee: {bitter: 1}
-            }
+            default_measures: {"ground_coffee": 18, "water": 36},
+            change_coeficients: {"ground_coffee": {"bitter": 1}}
         }]
         
-        this.createRecipes = this.createRecipes.bind(this)
+        
         this.recipeAccess = []
-        for (var i = 0; i < this.state.json.length; i++) {
-            this.recipeAccess[i] = React.createRef()
-        }
+        this.createRecipes = this.createRecipes.bind(this)
         this.update = this.update.bind(this)
-        this.state.recipes = this.createRecipes()
+        this.addRecipe = this.addRecipe.bind(this)
+        this.deleteRecipe = this.deleteRecipe.bind(this)
+        for (var i = 0; i < this.state.json.length; i++) {
+            this.state.recipeAccess[i] = React.createRef()
+        }
+        this.state.recipes = []
+        for (var i = 0; i < this.state.json.length; i++){
+            this.state.recipes[i] = (<Recipe ref={this.state.recipeAccess[i]} json={this.state.json[i]} key={i} father={this}/>)
+        }
+       
     }
     update() {
-        let prev = this.state.json
+        let prev = this.state
+        this.createRecipes()
         prev.json = []
         for (let i = 0; i < this.state.recipes.length; i++) {
-            let currentEditor = this.recipeAccess[i].current
+            let currentEditor = this.state.recipeAccess[i].current
             prev.json[i] = currentEditor.state.json
         }
         this.setState(prev)
     }
     createRecipes(){
-        var recipe = []
-        for(let i = 0; i < this.state.json.length; i++){
-            recipe[i] = (<Recipe ref={this.recipeAccess[i]} json={this.state.json[i]} key={i} father={this}/>)
+        let prev = this.state
+        for (var i = 0; i < prev.json.length; i++) {
+            prev.recipeAccess[i] = React.createRef()
         }
-        return recipe
+        prev.recipes =[]
+        for(let i = 0; i < prev.json.length; i++){
+            prev.recipes[i] = (<Recipe ref={prev.recipeAccess[i]} json={prev.json[i]} key={i} father={this}/>)
+        }
+        this.setState(prev)
+    }
+    deleteRecipe(i){
+        let prev = this.state
+        prev.json.revome(i)
+        this.setState(prev)
+        this.update()
+    }
+    addRecipe(){
+        let prev = this.state
+        prev.json[this.state.json.length] = [{name: "", 
+            tastes: [],
+            default_measures: {},
+            change_coeficients: {}}]
+        this.setState(prev)
+        this.update()
     }
     render() {
         return (
         <div>
-        <button>Добавить рецепт</button>
+        <button onClick={this.addRecipe}>Добавить рецепт</button>
         <form>
             {this.state.recipes}
             <input type='submit'></input>
