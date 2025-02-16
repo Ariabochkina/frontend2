701f6679f708113384c11af890c72b9be0684802
Amriabochkina <Anna.rm07@mail.ru>
2025-02-16 20:21:28 +0300

    fix add and delete

--- src/Components/Recipe.js
+++ src/Components/Recipe.js
@@ -6,21 +6,22 @@ export class Recipe extends Component {
         this.state = {json:null, father:null}
         this.state.json = props.json
         this.state.father = props.father
+        this.state.onDelete = props.onDelete
         this.changeName = this.changeName.bind(this)
     }
     changeName(event){
         var prevState = this.state
         prevState.json.name = event.target.value
         this.setState(prevState)
-        this.state.father.update()
     }
     render() {
         return (
         
             <label>
+                <button onClick={this.state.onDelete}>Удалить рецепт</button>
                 <p>Имя рецепта</p>
                 <textarea value={this.state.json.name} onChange={this.changeName}/>
-                </label>
+            </label>
         
         )
     }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,73 +1,59 @@
 import React, { Component } from 'react'
 import Recipe from '../Components/Recipe'
+const showRecipes = (props, onDelete) => {
+    return (<div>
+        {props.json.map(el => (
+            <Recipe json={el} key={el.id} onDelete={() => onDelete(el.id)}/>
+        ))}
+    </div>)
+}
 export class Home extends Component {
     constructor(props){
         super(props)
-        this.state = {json: null, recipes: null, recipeAccess: []}
+        this.state = {json: null}
         this.state.json = [{
+            id: 0,
             name: "espresso", 
             tastes: ["bitter", "sour", "strong"],
             default_measures: {"ground_coffee": 18, "water": 36},
             change_coeficients: {"ground_coffee": {"bitter": 1}}
-        }]
-        
-        
+        }]        
         this.recipeAccess = []
-        this.createRecipes = this.createRecipes.bind(this)
-        this.update = this.update.bind(this)
         this.addRecipe = this.addRecipe.bind(this)
         this.deleteRecipe = this.deleteRecipe.bind(this)
-        for (var i = 0; i < this.state.json.length; i++) {
-            this.state.recipeAccess[i] = React.createRef()
-        }
-        this.state.recipes = []
-        for (var i = 0; i < this.state.json.length; i++){
-            this.state.recipes[i] = (<Recipe ref={this.state.recipeAccess[i]} json={this.state.json[i]} key={i} father={this}/>)
-        }
        
     }
-    update() {
+    deleteRecipe(key){  
         let prev = this.state
-        this.createRecipes()
-        prev.json = []
-        for (let i = 0; i < this.state.recipes.length; i++) {
-            let currentEditor = this.state.recipeAccess[i].current
-            prev.json[i] = currentEditor.state.json
-        }
-        this.setState(prev)
-    }
-    createRecipes(){
-        let prev = this.state
-        for (var i = 0; i < prev.json.length; i++) {
-            prev.recipeAccess[i] = React.createRef()
-        }
-        prev.recipes =[]
-        for(let i = 0; i < prev.json.length; i++){
-            prev.recipes[i] = (<Recipe ref={prev.recipeAccess[i]} json={prev.json[i]} key={i} father={this}/>)
-        }
-        this.setState(prev)
-    }
-    deleteRecipe(i){
-        let prev = this.state
-        prev.json.revome(i)
-        this.setState(prev)
-        this.update()
+        prev.json = prev.json.filter(el => el.id !== key)
+        this.setState(prev) 
     }
     addRecipe(){
         let prev = this.state
-        prev.json[this.state.json.length] = [{name: "", 
-            tastes: [],
-            default_measures: {},
-            change_coeficients: {}}]
+        let len = prev.json.length
+        if (len === 0) {
+            prev.json[len] = {
+                id: 0,
+                name: "", 
+                tastes: [],
+                default_measures: {},
+                change_coeficients: {}}
+        } else {
+            prev.json[len] = {
+                id: prev.json[len - 1].id + 1,
+                name: "", 
+                tastes: [],
+                default_measures: {},
+                change_coeficients: {}}
+        }
         this.setState(prev)
-        this.update()
     }
     render() {
         return (
         <div>
         <button onClick={this.addRecipe}>Добавить рецепт</button>
         <form>
-            {this.state.recipes}
+            {showRecipes(this.state, this.deleteRecipe)}
             <input type='submit'></input>
         </form>
         </div>
